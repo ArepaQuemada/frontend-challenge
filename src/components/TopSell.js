58dc2cf639a315ec00474a26f4b9bb1252ea3a43
Juan <juan.rigual>
2020-09-06 19:56:12 -0300

    Added backdrop and loading

--- src/components/TopSell.js
+++ src/components/TopSell.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container, Typography, makeStyles, useMediaQuery } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, Typography, makeStyles, useMediaQuery, CircularProgress, Backdrop } from '@material-ui/core';
 import ItemCard from './ItemCard';
 import useFetch from '../utils/useFetch';
 import 'react-awesome-slider/dist/captioned.css';
@@ -8,26 +8,27 @@ import { Carousel } from '@techychan/react-responsive-carousel';
 
 const useStyles = makeStyles(theme => ({
     root: {
-        padding: '20px',
-        [theme.breakpoints.down("md")]: {
-            paddingTop: '10px',
-            paddingLeft: '10px'
-        }
+        padding: '20px'
     },
     background: {
-        backgroundColor: theme.palette.primary.main
+        backgroundColor: '#FFFFFF'
     },
     carouselContainer: {
         display: 'flex',
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
     }
 }));
 
 export default function TopSell() {
     const classes = useStyles();
+    const [loading, setLoading] = useState(false);
     const URL = 'https://corebiz-test.herokuapp.com/api/v1/products';
-    const response = useFetch(URL);
+    const response = useFetch(URL, setLoading);
     const { data = [] } = response || {};
-    const isMobile = useMediaQuery('(max-width:600px)');
+    const isMobile = useMediaQuery('(max-width:960px)');
 
     const parseData = (data, split) => {
         let interval = 0;
@@ -46,6 +47,14 @@ export default function TopSell() {
     }
 
     const parsedItems = isMobile ? parseData(data, 2) : parseData(data, 4);
+
+    if (loading) {
+        return (
+            <Backdrop open={loading} className={classes.backdrop}>
+                <CircularProgress color="secondary" />
+            </Backdrop>
+        );
+    }
 
     return (
         <Container className={classes.root} maxWidth="md">
