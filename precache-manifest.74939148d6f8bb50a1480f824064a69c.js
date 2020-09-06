84f1b8c7f1dd7fedf4adb2e1a745c6d3a32ac15f
Juan <juan.rigual>
2020-09-06 15:51:16 -0300

    Updates

---
+++ precache-manifest.74939148d6f8bb50a1480f824064a69c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9533969f59cd498853fa8c83d07b23cd",
+    "url": "/frontend-challenge/index.html"
+  },
+  {
+    "revision": "84032adcd805e23b5642",
+    "url": "/frontend-challenge/static/css/2.6938f16a.chunk.css"
+  },
+  {
+    "revision": "c903524337b29939f9a3",
+    "url": "/frontend-challenge/static/css/main.797d8cea.chunk.css"
+  },
+  {
+    "revision": "84032adcd805e23b5642",
+    "url": "/frontend-challenge/static/js/2.8eb2710e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/frontend-challenge/static/js/2.8eb2710e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c903524337b29939f9a3",
+    "url": "/frontend-challenge/static/js/main.f307e418.chunk.js"
+  },
+  {
+    "revision": "a6301b192720c2fe64bd",
+    "url": "/frontend-challenge/static/js/runtime-main.516e6923.js"
+  },
+  {
+    "revision": "cfc6af473a1d3511a8f9816a92cf6409",
+    "url": "/frontend-challenge/static/media/desktop-banner.cfc6af47.jpg"
+  },
+  {
+    "revision": "db8102f4490617b4268501bc0dfaaf85",
+    "url": "/frontend-challenge/static/media/logo.db8102f4.svg"
+  },
+  {
+    "revision": "303001bb83c0905b2611e781a0a62300",
+    "url": "/frontend-challenge/static/media/mobile-banner.303001bb.jpg"
+  }
+]);
