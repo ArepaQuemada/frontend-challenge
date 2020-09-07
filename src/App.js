0ec7baa7f21d04585353e5521e7bde0633e2c039
Juan <juan.rigual>
2020-09-07 13:02:15 -0300

    Added local storage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import PageCarousel from './components/PageCarousel';
 import TopSell from './components/TopSell';
 import FormNews from './components/FormNews';
 import Footer from './components/Footer';
 
-export const ItemsCartContext = React.createContext('');
+export const ItemsCartContext = React.createContext(0);
 
 function App() {
 
-  const [ itemsCart, updateItemsCart ] = useState(0);
+  const [ itemsCart, updateItemsCart ] = useState(JSON.parse(localStorage.getItem('value')))
+
+  useEffect(() => {
+    localStorage.setItem('value', JSON.stringify(itemsCart))
+  }, [itemsCart])
 
   return (
     <div className="App">
